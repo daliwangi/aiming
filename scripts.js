e0929e958b6ffbddfdf71f0a7806d291c24c6110
Dali King <Dali King>
2017-04-02 17:30:46 +0800

    Right click support and console statistics added.

--- scripts.js
+++ scripts.js
@@ -30,6 +30,7 @@ $(document).ready(function(){
 	var totalShots = 0;
 	var totalHits = 0;
 	var targetOnDelay = 0;
+	var hitrate = 0;
 	var winWidth = $(window).width();
 	var winHeight = $(window).height();
 	
@@ -39,11 +40,28 @@ $(document).ready(function(){
 		configs = $('form').serializeObject();
 		console.log(configs);
 		$('#configs').fadeOut(500);
+		$('#backdrop').width(winWidth);
+		$('#backdrop').height(winHeight);
 		
 		targetInterval = setInterval(function(){ nextTarget(); started = true; } ,100);
 	});
 
 	$(document).click(function(e) {
+		misses += 1;
+		if (started)
+		{
+			totalShots ++;
+			if (configs.enableSound)
+			{
+				shotSound.currentTime=0;
+				shotSound.play();
+			}
+		}
+	});
+	
+	$(document).on('contextmenu','#backdrop',function(e) {
+		e.preventDefault();
+		misses += 1;
 		if (started)
 		{
 			totalShots ++;
@@ -55,10 +73,29 @@ $(document).ready(function(){
 		}
 	});
 
-	$('body').on('click','.target',(function(){	
-		console.log('hit');	
-		targetShot = (new Date()).getTime();
+	$('#backdrop').on('click','.target',(function(){	
+		misses -= 1;
 		totalHits++;
+		hitrate = totalHits/(misses + totalHits);
+		console.log("hit rate is");
+		console.log(hitrate);
+		targetShot = (new Date()).getTime();		
+		delay = targetShot - $(this).data('shown');
+		totalDelay += delay;
+		mediumDelay = totalDelay / totalShots;
+		if (delay > maxDelay) { maxDelay = delay; }
+		if (delay < minDelay) { minDelay = delay; }
+		$(this).remove();
+	}));
+	
+	$('#backdrop').on('contextmenu','.target',(function(e){	
+		e.preventDefault();//阻止右键菜单
+		misses -= 1;
+		totalHits++;
+		hitrate = totalHits/(misses + totalHits);
+		console.log("hit rate is");
+		console.log(hitrate);
+		targetShot = (new Date()).getTime();		
 		delay = targetShot - $(this).data('shown');
 		totalDelay += delay;
 		mediumDelay = totalDelay / totalShots;
@@ -69,7 +106,6 @@ $(document).ready(function(){
 
 	function nextTarget()
 	{
-		console.log('nextTarget()');
 		targetCount = $('.target').length;
 		if ((targetCount+targetOnDelay) === 0) { showTarget(); }
 		if (configs.multipleTargets && (targetCount+targetOnDelay < 3) && (rand(1,5) == 1)) { showTarget(); }
@@ -78,19 +114,18 @@ $(document).ready(function(){
 	function showTarget()
 	{
 		targetOnDelay++;
-		console.log('showTarget()');
 		delay = (configs.randomDelay)? rand(1,mediumDelay): 1;
 		setTimeout(function()
 		{
 			targetShown = (new Date()).getTime();
-			target = $('<div id="'+targetShown+'" class="target" data-shown="'+ targetShown +'" />');
+			target = $('<div id="'+targetShown+'" class="target" data-shown="'+ targetShown +'" />');			
 
 			target.css('top',rand(1, winHeight-30)); 
 			target.css('left',rand(1, winWidth-30));
 			if (configs.randomSize) { target.width(rand(10,30)); target.height(rand(10,30)); }
-			else { target.width(15); target.height(15); }
+			else { target.width(10); target.height(10); }
 
-			$('body').append(target);
+			$('#backdrop').append(target);
 
 			if (configs.movingTarget)
 			{
